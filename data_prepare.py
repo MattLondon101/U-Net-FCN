25bdedbbfd9d158b223b791ef377668698c5ee3d
Matt London <matthewlondon101@gmail.com>
2021-06-06 22:43:24 -0400

    Update data_prepare.py

--- data_prepare.py
+++ data_prepare.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 import time
 start_time=time.time()
 
-# exec(open('imports.py').read())
+exec(open('imports.py').read())
 
 from importlib import reload
 import data
@@ -22,10 +22,8 @@ reload(model)
 import model
 from model import *
 
-
-exec(open('imports.py').read())
-
-# move matching labels to new dir
+# When picking specific non-consecutive images from directory, func:labmv will select labels that correspond with images and copy to new directory.
+# written for Carvana dataset (https://www.kaggle.com/c/carvana-image-masking-challenge/data) where jpg images and gif labels have identical names, except labels end with '_mask'
 def labmv(srcim,srclab,outlab):
     for fi in natsorted(os.listdir(srcim),alg=ns.PATH):
         fp=os.path.join(srcim,fi)
@@ -38,10 +36,10 @@ def labmv(srcim,srclab,outlab):
                     lbfi=os.path.join(srclab,lfi)
                     shutil.copy(lbfi,os.path.join(outlab,lfi))
 
-labmv('datadir/train/ims_cars_name','datadir/train/train_masks_all','datadir/train/labels_cars_jpg_name')
+labmv('datadir/train/ims','datadir/train/labels_all','datadir/train/labels_gif')
 
 
-# rename ims to num
+# rename named ims to num
 def rimn(src,ext):
     i=0
     for fi in natsorted(os.listdir(src),alg=ns.PATH):
@@ -50,7 +48,7 @@ def rimn(src,ext):
             fop=os.path.join(src,str(i)+str(ext))
             os.rename(fp,fop)
             i+=1
-rimn('datadir/train/ims_cars_num','.jpg')
+rimn('datadir/train/ims','.jpg')
 
 
 # 3D gif to 2D jpg
@@ -69,7 +67,7 @@ def rdgif(src,out,ext):
             fop=os.path.join(out,nfi)
             cv.imwrite(fop,ar[:,:,0])
             i+=1
-rdgif('datadir/train/labels_cars_jpg_name','datadir/train/labels_cars_jpg_num','.jpg')
+rdgif('datadir/train/labels_gif','datadir/train/labels_jpg','.jpg')
 
 
 # resize imgs and labels to 256 x 256 x 3 pixels
@@ -82,13 +80,13 @@ def rsiz(src):
             height=256
             rim=im.resize((width,height),Image.NEAREST)
             rim.save(fp)
-rsiz('datadir/train/ims_cars_num')
-rsiz('datadir/train/labels_cars_jpg_num')
+rsiz('datadir/train/ims')
+rsiz('datadir/train/labels_jpg')
 
 
 # check image shape
-im=cv.imread('datadir/train/ims_cars_num/2.jpg')
+im=cv.imread('datadir/train/ims/2.jpg')
 im.shape
-lab=cv.imread('datadir/train/ims_cars_num/2.jpg')
+lab=cv.imread('datadir/train/labels_jpg/2.jpg')
 lab.shape
 
